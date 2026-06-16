64f1469aa638be50296d98b3520f2254584d1696
Miracle Otugo <67126036+MirakoolDev@users.noreply.github.com>
2026-06-16 12:42:05 +0100

    Update layout.tsx

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -18,6 +18,7 @@ export const metadata: Metadata = {
     "An open-edition NFT survival game. Mint a token. Cuts happen every round. The final 4 share the prize pool.",
   other: {
     "base:app_id": "6a238e9711cac27e581f114e",
+    "talentapp:project_verification": "4053361357a3a3048c9d64e1f1769bb29978ad3337f9d977bd32a99ccafcb76874e189f13a839828a05fd226ea0f846c609464e0ba7f6f5eb46cc8c5fcf82b68"
   },
 };
 
